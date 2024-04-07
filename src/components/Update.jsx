d4e437cc97c85ffe4a3d3854c0886e97aba523ea
Geomon babu <megeomonbabu@gmail.com>
2024-04-07 16:14:11 +0530

    completed reglogin

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -1,33 +1,50 @@
-import React from 'react'
 import Footer from './Footer'
 import Header from './Header'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const Update = () => {
+  const [input,setInput] = new useState(
+    {             
+    "userId":sessionStorage.getItem("userid")
+    }
+)
+const [data, setData] = new useState([])
+  const getData = () => {
+      axios.post("http://localhost:3001/api/eduapp/updateprofile",input).then(
+          (response) => {
+              setData(response.data)
+              console.log(response.data)
+          }
+      )
+  }
+  useEffect(() => { getData() }, [])
   return (
     <div>
         <Header />
-        <section clasName="form-container">
-
-<form action="" method="post" enctype="multipart/form-data">
-   <h3>update profile</h3>
-   <p>update name</p>
-   <input type="text" name="name" placeholder="shaikh anas"  maxLength="50" clasName="box" />
-   <p>update email</p>
-   <input type="email" name="email" placeholder="shaikh@gmail.come"  maxLength="50" clasName="box" />
-   <p>previous password</p>
-   <input type="password" name="old_pass" placeholder="enter your old password"  maxLength="20" clasName="box" />
-   <p>new password</p>
-   <input type="password" name="new_pass" placeholder="enter your old password"  maxLength="20" clasName="box" />
-   <p>confirm password</p>
-   <input type="password" name="c_pass" placeholder="confirm your new password"  maxLength="20" clasName="box" />
-   <p>update pic</p>
-   <input type="file" accept="image/*" clasName="box" />
-   <input type="submit" value="update profile" name="submit" clasName="btn" />
-</form>
-
-</section>
-<Footer />
-    </div>
+        <section className="form-container align-center">
+        
+       <form action="" method="" enctype="multipart/form-data">
+                    <h3>update profile</h3>
+                    <p>update name<span>*</span></p>
+                    <input type="text" name="name" placeholder="Enter your password" required maxLength="20" className="box" />
+                    <p>update email</p>
+                    <input type="email" name="email" value=""required maxLength="20" className="box" />
+                    <p>previous password</p>
+                    <input type="password" name="old_pass"value="" placeholder="Enter your password" required maxLength="20" className="box" />
+                    <p>new password</p>
+                    <input type="password" name="new_pass" placeholder="Enter your password" required maxLength="20" className="box" />
+                    <p>confirm password</p>
+                    <input type="password" name="c_pass" placeholder="Enter your password" required maxLength="20" className="box" />
+                    <p>Select Profile Image <span>*</span></p>
+                    <input type="file" name="profileimage" required className="box"  />
+                    <input type="button" value="Register New" className="btn"  />
+           </form>
+           
+        </section>
+        <Footer />
+   </div>
   )
 }
 
